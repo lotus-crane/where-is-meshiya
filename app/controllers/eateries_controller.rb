2b917d42948684e5a5d3092cc58f3c57c5c255fd
Ryo Tsuruhashi <s1711543@s.tsukuba.ac.jp>
2019-05-17 16:22:47 +0900

    フォームの入力を反映する

--- app/controllers/eateries_controller.rb
+++ app/controllers/eateries_controller.rb
@@ -1,7 +1,15 @@
 class EateriesController < ApplicationController
-  def change
-    create_table :eateries do |t|
-      t.string :name
-    end
+  def create
+    eatery = Eatery.new(eatery_params)
+    eatery.save
+    flash[:success] = "#{eatery.name}を追加しました"
+    redirect_to root_path
   end
+
+  private
+  
+  def eatery_params
+    params.require(:eatery).permit(:name)
+  end
+
 end
