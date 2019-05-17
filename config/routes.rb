e798916ff57bf7c6f01b64755b30bd8677b0adaa
Ryo Tsuruhashi <s1711543@s.tsukuba.ac.jp>
2019-05-17 17:36:16 +0900

    ユーザログイン機能追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
   post 'eateries/create'
   get 'eateries/new'
   get 'lunch/go'
+  get '/login',to: 'sessions#new'
+  post 'sessions/create'
   root 'lunch#go'
 end
